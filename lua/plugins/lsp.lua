8a223247cf6fe3ea0a32085c15cae848108ba1dd
Saulo Leao <sleaojr@gmail.com>
2026-07-24 13:31:36 -0300

    update config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,14 @@ return {
           mason = false,
           autostart = false,
         },
+        -- Workaround for terraform-ls hang on Neovim 0.12 caused by multiline
+        -- semantic tokens (deltaStartChar overflow). See hashicorp/terraform-ls#2122.
+        -- Remove once a terraform-ls release includes the fix.
+        terraformls = {
+          on_attach = function(client, _)
+            client.server_capabilities.semanticTokensProvider = nil
+          end,
+        },
       },
     },
   },
